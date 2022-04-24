a6a6ce6681be1a4278a054497c8ec1185c58a258
Love6 <2251152361@qq.com>
2022-04-25 02:25:50 +0800

    Fixed Gettid calls successfully

--- Util/thread.cc
+++ Util/thread.cc
@@ -15,9 +15,13 @@ __thread int t_cachedTid = 0;
 __thread char t_formattedTid[32];
 __thread int t_formattedTidLength;
 
+pid_t gettid() {
+  return static_cast<int>(syscall(SYS_gettid));
+}
+
 void CacheTid() {
   if (t_cachedTid == 0) {
-    t_cachedTid = ::gettid();
+    t_cachedTid = gettid();
     t_formattedTidLength = snprintf(t_formattedTid, sizeof(t_formattedTid), "%5d ", t_cachedTid);
   }
 }
